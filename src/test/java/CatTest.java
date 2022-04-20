6495655a83f4b6d048341a06434a6fd5704ef1b6
Saida Karakhanova <sayakrh@yandex.kz>
2022-04-20 10:01:40 +0600

    Sprint 2.1

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,39 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest extends Feline {
+
+    @Mock
+    Feline feline;
+
+    Cat cat = new Cat(feline);
+
+    @Test
+    public void catSoundTest() {
+        String actualSound = cat.getSound();
+        assertEquals("Мяу", actualSound);
+    }
+
+    @Test
+    public void shouldCatHasFoodList() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> mocList = Arrays.asList("Животные", "Птицы", "Рыба");
+        Mockito.when(cat.getFood()).thenReturn(mocList);
+        List<String> actualList = cat.getFood();
+        List<String> expectedList = Arrays.asList("Животные", "Птицы", "Рыба");
+        assertEquals(expectedList, actualList);
+    }
+
+}
---
+++ src/test/java/LionHasManeWithParamTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+
+@RunWith(Parameterized.class)
+public class LionHasManeWithParamTest {
+
+
+    String sex;
+    Boolean hasMane;
+
+    public LionHasManeWithParamTest(String sex, Boolean hasMane){
+        this.sex=sex;
+        this.hasMane=hasMane;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getHasMane() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+    /**
+     * Проверяем гриву.
+     */
+    @Test
+    public void shouldMaleLionHasMane() throws Exception {
+        Lion lion = new Lion(sex, new Feline());
+        assertEquals(lion.doesHaveMane(), hasMane);
+    }
+
+}
